b78233186bdca0a50a52af882e295e3bd88efba8
Tarun Raja <tarun.deadhead@gmail.com>
2013-07-19 12:59:04 +0530

    Finish layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,26 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  subject {page}
+
   let(:base_title) { "Project1" }
     
   describe "Home page" do
-    it "should have the content 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Home')  
-    end
-    
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
-    end
+    before { visit root_path }
+    it {should have_content('Home')}  
+    it {should have_title("#{base_title} | Home")}
   end
 
-  describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+  describe "Contact page" do
+    before {visit contact_path}
+    it {should have_content('Contact')}
+    it {should have_title("#{base_title} | Contact")}
   end
 
   describe "About Us page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/aboutus'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/aboutus'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    before {visit aboutus_path}  
+    it {should have_content('About Us')}
+    it {should have_title("#{base_title} | About Us")}
   end
 
 end
